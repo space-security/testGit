985853cd27d5af76be8dfdefd83d6cbc5620479a
space-security <a18703189601@163.com>
2022-03-17 22:27:34 +0800

    test1.cpp

---
+++ test1.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+ @Author: 
+ @Created Time : 2022年03月17日 星期四 22时22分20秒
+ @File Name: test1.cpp
+ @Description:
+ ************************************************************************/
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "hello, world" << endl;
+    return 0;
+}
